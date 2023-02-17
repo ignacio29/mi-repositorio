0101d95ffa47f21229ca68f1ee09fb1e131c78b9
Ignacio29 <joseignacioalfarorodriguez85@gmail.com>
2023-02-16 19:54:53 -0600

    agregar archivo index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+//function suma (a, b){
+ //   return a +b;
+//}
+
+/*const suma1 = (a,b) =>{
+    console.log(a,b);
+    return a + b
+}
+const suma = (a,b) => a + b
+
+const resultado=suma(1,2);
+
+console.log(resultado);*/
+
+/*function sinPromesa() {
+    return Promise.resolve(1)
+    .then(x => x + 1)
+    .then(x => x + 1)
+    .then(console.log)
+}*/
+
+//sinPromesa()
+
+async function conPromesa(){
+    const resultado = await Promise.resolve(1)
+    console.log(resultado + 1 + 1);
+}
+
+conPromesa()
